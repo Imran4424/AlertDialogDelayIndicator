1993b600048d293ef113b81d103e20f61139bb7e
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-01-31 23:06:09 +0600

    refactor to home view

--- AlertDialogDelayIndicator/AlertDialogDelayIndicatorApp.swift
+++ AlertDialogDelayIndicator/AlertDialogDelayIndicatorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AlertDialogDelayIndicatorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
