043497bb5f4679ef1cdc29e41f812c307cca8f4a
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-01-31 23:00:09 +0600

    project init

---
+++ AlertDialogDelayIndicator/AlertDialogDelayIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertDialogDelayIndicatorApp.swift
+//  AlertDialogDelayIndicator
+//
+//  Created by Shah Md Imran Hossain on 31/1/24.
+//
+
+import SwiftUI
+
+@main
+struct AlertDialogDelayIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
